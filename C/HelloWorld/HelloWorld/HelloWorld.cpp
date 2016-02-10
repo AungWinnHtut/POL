6694f053833a5bb7cd8981c614e52efc2e621ed4
Dr.Aung Win Htut <aungwinnhtut@gmail.com>
2016-02-10 16:47:12 +0630

    Hello World

---
+++ C/HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h> //_getch()
+
+int main()
+{
+
+	printf("Hello World\n"); // \n = new line char
+	printf("I am aung win  htut\n");
+	printf("Testing\n");
+	printf("============\n");
+	printf("1-Help\n");
+	printf("2-Open\n");
+	printf("3-Exit");
+	_getch();
+	return 0;
+}
