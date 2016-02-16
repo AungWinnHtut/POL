966267f34f3205db94c9af18c380a22b29e29cd4
Dr.Aung Win Htut <aungwinnhtut@gmail.com>
2016-02-16 17:48:30 +0630

    del *.apk and add hello.c and makefile

---
+++ C/hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+	cout<<"hello world"<<endl;
+	system("dir");
+	getch();
+}
--- C/shell/shell/shell.cpp
+++ C/shell/shell/shell.cpp
@@ -1,10 +1,67 @@
 #include<stdio.h>
 #include<conio.h>
+//nclude <windows.h>
+//#include <tchar.h>
 #include<process.h> //system()
+
 int main()
 {
-	system("dir g:\\ >> g:\\systeminfo2.txt");
-	system("notepad g:\\systeminfo2.txt");
+	int iAns;
+	system("cls");
+	printf("Advanced APK installer\n");
+	printf("======================\n");
+	printf("1 - Games\n");
+	printf("2 - Musics\n");
+	printf("3 - Exit\n");
+	printf("Please Choose One of this  ");
+	scanf("%d",&iAns);
+	//printf("the ans is %d",iAns);
+	if(iAns==1)
+	{
+		system("adb install game.apk");
+	}
+	if(iAns==2)
+	{
+		system("adb install music.apk");
+	}
+	if(iAns==3)
+	{
+		exit(0);
+	}
+
 	_getch();
 	return 0;
 }
+
+
+
+
+
+//void test()// _tmain(int argc, TCHAR *argv[])
+//{
+//   WIN32_FIND_DATA FindFileData;
+//   HANDLE hFind;
+//   argc==2;
+//   //if( argc != 2 )
+//   //{
+//   //   _tprintf(TEXT("Usage: %s [target_file]\n"), argv[0]);
+//	  //_getch();
+//   //   return;
+//   //}
+//   argv[1]="*.apk";
+//   _tprintf (TEXT("Target file is %s\n"), argv[1]);
+//   hFind = FindFirstFile(argv[1], &FindFileData);
+//   if (hFind == INVALID_HANDLE_VALUE) 
+//   {
+//      printf ("FindFirstFile failed (%d)\n", GetLastError());
+//	  _getch();
+//      return;
+//   } 
+//   else 
+//   {
+//      _tprintf (TEXT("The first file found is %s\n"), 
+//                FindFileData.cFileName);
+//      FindClose(hFind);
+//   }
+//   _getch();
+//}
