418f23cd6b18f2dedeb50a08ba07043f96362585
Dr.Aung Win Htut <aungwinnhtut@gmail.com>
2019-11-09 14:30:41 +0630

    add hanzawnyein's first code

---
+++ students.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+
+{
+    float radius,area;
+
+    printf("enter the radius");
+    scanf ("%f",&radius);
+    area = 3.143433 *radius *radius;
+    printf ("Area = %f", area);
+}
