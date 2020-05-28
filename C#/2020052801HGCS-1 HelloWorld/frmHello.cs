408546bafbf4d1d213f137893f78f1b1e709e819
Dr.Aung Win Htut <aungwinnhtut@gmail.com>
2020-05-28 14:21:24 +0630

    add new hello world for hgcs

---
+++ C#/2020052801HGCS-1 HelloWorld/frmHello.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2020052801HGCS_1_HelloWorld
+{
+    public partial class frmHello : Form
+    {
+        public frmHello()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHello_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World!","Green Hackers",MessageBoxButtons.YesNoCancel);
+        }
+    }
+}
